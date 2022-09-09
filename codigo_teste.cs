9ea971e539c0b8a20aa9fc9cf74a854c431c911c
pcomenda <101903885+pcomenda@users.noreply.github.com>
2022-09-09 16:07:18 -0300

    Add files via upload

---
+++ codigo_teste.cs
@@ -0,0 +1,13 @@
+int lado_1 = Console.ReadLine();
+int lado_2 = Console.ReadLine();
+int lado_3 = Console.ReadLine();
+
+if(lado_1 == lado_2 && lado_1 == lado_3){
+    Console.WriteLine("O triângulo é Equilátero");
+}
+else if(lado_1 == lado_2 || lado_1 == lado_3){
+    Console.WriteLine("O triângulo é Isóceles");
+}
+else{
+    Console.WriteLine("O triângulo é Escaleno");
+}
